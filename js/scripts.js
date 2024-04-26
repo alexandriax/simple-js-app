f4f6756151cd0ad2a5182051172a67f122841c69
alexandriax <allyhamilton0@gmail.com>
2024-04-25 21:04:42 -0400

    add iife /js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -41,7 +41,48 @@ for(let i=0; i<pokemonList.length; i++) {
 */
 
 
-pokemonList.forEach(function(pokemon) {
+let pokemonList = (function () {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 5,
+      types: ['grass', 'poison'],
+    },
+
+    {
+      name: 'Ninetales',
+      height: 7,
+      types: ['fire'],
+    },
+
+    {
+      name: "Gengar",
+      height: 4,
+      types: ['ghost', 'poison'],
+    },
+
+    {
+      name: 'Ditto',
+      height: 1,
+      types: ['normal'],
+    }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonList.getAll().forEach(function(pokemon) {
   document.write(pokemon.name + ' height: ' + pokemon.height + ' ')
   if (pokemonList.height <=2) {
     document.write(' That\'s a tiny pokemon! ');
